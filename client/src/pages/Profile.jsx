bdaf0b47d010458dd57e3042a9e0ec1fcce9fc19
James-wasonga <jameswasonga22@gmail.com>
2024-07-11 20:09:33 +0300

    display the owners campaign on the profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,30 +1,99 @@
+// import React, { useState , useEffect} from 'react';
+
+// import { DisplayCampaigns } from '../components';
+// import { useStateContext } from '../context';
+
+// const Profile = () => {
+//   const [isLoading , setIsLoading] = useState(false);
+//   const [campaigns, setCampaigns] = useState([]);
+//   //Aiming at displaying the fetch data from the smart contract onto the homePage
+
+// const {address, contract, getUserCampaigns} = useStateContext();
+
+// const fetchCampaigns = async () => {
+//   setIsLoading(true);
+//   const data = await getUserCampaigns();
+//   setCampaigns(data);
+//   setIsLoading(false);
+// }
+
+
+// useEffect(() =>{
+//   if(contract) fetchCampaigns; 
+// },[address, contract]);
+
+//   return (
+//     <DisplayCampaigns
+//     title='All my Compaigns'
+//     isLoading = {isLoading}
+//     campaigns = {campaigns}
+//   />
+
+//   )
+// }
+
+// export default Profile
+
+
 import React, { useState , useEffect} from 'react';
 
 import { DisplayCampaigns } from '../components';
 import { useStateContext } from '../context';
+import { useAppContext } from '../providers/AppProvider';
 
-const Profile = () => {
+
+  const Profile = () => {
   const [isLoading , setIsLoading] = useState(false);
   const [campaigns, setCampaigns] = useState([]);
   //Aiming at displaying the fetch data from the smart contract onto the homePage
 
-const {address, contract, getUserCampaigns} = useStateContext();
+//const {address, contract, getCampaigns} = useStateContext();
+  const {contract, address, provider} = useAppContext();
 
-const fetchCampaigns = async () => {
-  setIsLoading(true);
-  const data = await getUserCampaigns();
-  setCampaigns(data);
-  setIsLoading(false);
-}
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    // const data = await getCampaigns();
+    // setCampaigns(data);
+    // setIsLoading(false);
 
+    try{
+      const campaignsData = await contract.get_campaigns();
+      setCampaigns(campaignsData);
+      console.log("Fetched Campaigns Successfully",campaignsData);
+  
+      // const userCampaigns = campaignsData.filter(campaign => campaign.owner === address);
+      // setCampaigns(userCampaigns);
+      // console.log("User Campaigns:", userCampaigns);
+    } catch (error){
+      console.error("Error fetching campaign:", error);
+
+    } finally{
+      setIsLoading(false);
+    }
+
+    };
+
+
+
+//   const myCall = contract.populate('get_campaigns', [])
+//     setIsLoading(true)
+//     contract['get_campaigns'](myCall.calldata).then((res) => {
+//         console.info("Successful Response:", res)
+//     }).catch((err) => {
+//         console.error("Error: ", err)
+//     }).finally(() => {
+//         setIsLoading(false)
+//     })
+
+// }
 
 useEffect(() =>{
-  if(contract) fetchCampaigns; 
-},[address, contract]);
+  if(contract) fetchCampaigns(); 
+},[contract, address]);
 
   return (
     <DisplayCampaigns
-    title='All my Compaigns'
+    title='All Compaigns'
     isLoading = {isLoading}
     campaigns = {campaigns}
   />
@@ -32,4 +101,4 @@ useEffect(() =>{
   )
 }
 
-export default Profile
+ export default Profile;
