fee7494b749194d85907ab088234309e7316ff52
James-wasonga <jameswasonga22@gmail.com>
2024-07-11 20:04:41 +0300

    Display campaign details on the home FundCard

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { bigintToLongAddress } from '../AppUtils';
-import { bigintToShortStr } from '../AppUtils';
+import { bigintToLongAddress } from '../utils';
+import { bigintToShortStr } from '../utils';
 
 import FundCard from './FundCard';
 import { loader } from '../assets';
@@ -33,14 +33,17 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
         {!isLoading && campaigns.length > 0 && campaigns.map((campaign) =>(
            <FundCard
           key={campaign.id}
-          owner={bigintToLongAddress(campaign.owner).slice(0,10)+"..."+bigintToLongAddress(campaign.owner).slice(-6)}
+          owner={bigintToLongAddress(campaign.owner).slice(0,16)+"..."+bigintToLongAddress(campaign.owner).slice(-6)}
           title={bigintToShortStr(campaign.title)}
-          description={bigintToShortStr(campaign.description)}
+          description={campaign.description}
           target={campaign.target.toString()}
           amountCollected={campaign.amount_collected.toString()}
-          image={bigintToShortStr(campaign.image)}
+          image={campaign.image}
+          deadline={campaign.deadline}
+      
 
-             //{...campaign}
+
+            // {...campaigns}
             handleClick={() => handleNavigate(campaign)}
 
           />
