7092a32563f560f27c30d3d7d63b367c90cdc9b5
James-wasonga <jameswasonga22@gmail.com>
2024-07-19 17:38:32 +0300

    change owner icon

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {tagType, thirdweb, starkraise} from '../assets';
+import {tagType, thirdweb, starkraise, stark} from '../assets';
 import{ daysLeft } from '../utils';
 import { bigintToLongAddress } from '../utils';
 import { bigintToShortStr } from '../utils';
@@ -39,7 +39,7 @@ const FundCard = ({owner,title, description, target, deadline, amountCollected, 
 
         <div className='flex items-center mt-[20px] gap-[12px]'>
             <div className='w-[30px] h-[30px] rounded-full flex justify-center items-center bg-black'>
-                <img src={starkraise} alt="user" className='w-1/2 h-1/2 object-contain' />
+                <img src={stark} alt="user" className='w-1/2 h-1/2 object-contain' />
             </div>
             <p className='flex-1 font-epilogue font-normal text-[12px] text-[#808191]'> by <span className='text-[#b2b3bd]'>{owner}</span>
 
