dfa0148977b54d0c97558af0a125f1aa6aac5e4d
James-wasonga <jameswasonga22@gmail.com>
2024-07-27 14:32:38 +0300

    export welcome page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,5 +2,7 @@ export { default as Home } from './Home';
 export { default as Profile } from './Profile';
 export { default as CreateCampaign } from './CreateCampaign';
 export { default as CampaignDetails } from './CampaignDetails';
+export { default as WelcomePage } from './WelcomePage';
+
 
 
