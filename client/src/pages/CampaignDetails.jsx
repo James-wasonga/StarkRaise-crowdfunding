5ea0da97b220b435d97e1e47c8ad7f21368f33fd
James-wasonga <jameswasonga22@gmail.com>
2024-08-04 21:45:11 +0300

    implement donation withdrawal on the frontend

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,27 +1,54 @@
-import React, {useState, useEffect} from 'react';
-import { useLocation , useNavigate } from 'react-router-dom';
-import { ethers } from 'ethers';
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
 
+import { useStateContext } from "../context";
+import { CustomButton, CountBox, Loader, Modal } from "../components";
 
-import { useStateContext } from '../context';
-import { CustomButton, CountBox, Loader  } from '../components';
-import { bigintToLongAddress, calculateBarPercentage, daysLeft} from '../utils';
-import { starkraise, thirdweb, stark } from '../assets';
-import { useAppContext } from '../providers/AppProvider';
+import {
+  bigintToLongAddress,
+  calculateBarPercentage,
+  daysLeft,
+} from "../utils";
+import { starkraise, thirdweb, stark } from "../assets";
+import { useAppContext } from "../providers/AppProvider";
 
 const CampaignDetails = () => {
   const { state } = useLocation();
-  console.log(state)
+  console.log(state);
   const navigate = useNavigate();
-  const{donate, getDonations , contract, address } = useAppContext();
+  const { donate, getDonations, contract, address, handleWalletConnection } =
+    useAppContext();
   const [isLoading, setIsLoading] = useState(false);
-  const [amount , setAmount] = useState(''); 
+  const [amount, setAmount] = useState("");
   const [donators, setDonators] = useState([]);
+  const [withdrawModal, setWithdrawModal] = useState(false);
+
+  const handleWithdrawFunction = () => {
+    const campaignId = state.id.toString();
+    console.log(campaignId);
+    if (contract && address) {
+      const myCall = contract.populate("withdraw_donations", [campaignId]);
+      setIsLoading(true);
+      contract["withdraw_donations"](myCall.calldata)
+        .then((res) => {
+          console.info("Successful Response:", res);
+        })
+        .catch((err) => {
+          console.error("Error: ", err);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    } else {
+      handleWalletConnection();
+    }
+  };
 
   const remainingDays = daysLeft(state.deadline);
-    console.log(state.amount_collected.toString());
+  console.log(state.amount_collected.toString());
 
-    console.log(contract)
+  console.log(contract);
 
   const fetchDonators = async () => {
     const data = await contract.get_donations(state.id.toString(), 1);
@@ -29,32 +56,28 @@ const CampaignDetails = () => {
     console.log(data);
 
     setDonators(data);
-  }
+  };
 
-  console.log(state.id)
+  console.log(state.id);
 
-  console.log(donators)
-
- 
+  console.log(donators);
 
   useEffect(() => {
-    if(contract) fetchDonators();
-  },[contract, address])
+    if (contract) fetchDonators();
+  }, [contract, address]);
 
   const handleDonate = async () => {
     setIsLoading(true);
     await donate(state.id, amount);
-    navigate('/');
-    setIsLoading(false);  
+    navigate("/");
+    setIsLoading(false);
+  };
 
-  }
+  return (
+    <div>
+      {isLoading && <Loader />}
 
-  
-return (
-  <div>
-    {isLoading && <Loader />}
-
-    <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+      <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
       <div className="flex-1 flex-col">
         <img src={state.image} alt="campaign" className="w-full h-[410px] object-cover rounded-xl"/>
         <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
@@ -100,50 +123,93 @@ return (
             <div className="mt-[20px] flex flex-col gap-4">
               {donators? donators.map((item, index) => (  
                 <div key={`${item.donor}-${index}`} className="flex justify-between items-center gap-4">
-                  <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">{index + 1}. {bigintToLongAddress(item.donor)}</p>
+                  <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">{index + 1}. {bigintToLongAddress(item.donor).slice(0,18) +"..."+bigintToLongAddress(item.donor).slice(-6) }</p>
                   <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">{item.amount?.toString() / 1000000000000000000} Eth</p>
                 </div>
               )) : (
                 <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">No donators yet. Be the first one!</p>
               )}
+              <Modal
+                isOpen={withdrawModal}
+                onClose={() => {
+                  setWithdrawModal(false);
+                }}
+              >
+                <h1 className="text-xl" style={{ fontWeight: "600" }}>
+                  Are you sure you want to withdraw?
+                </h1>
+                {/* <input
+                  type="number"
+                  placeholder="Amount"
+                  step="0.01"
+                  className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                /> */}
+                <br />
+                <br />
+                <CustomButton
+                  handleClick={() => {
+                    handleWithdrawFunction();
+                  }}
+                  btnType="button"
+                  title="withdraw"
+                  styles="w-full bg-[#8c6dfd]"
+                />
+              </Modal>
+              <CustomButton
+                btnType="button"
+                title="withdraw"
+                styles="w-full bg-[#8c6dfd]"
+                handleClick={() => {
+                  setWithdrawModal(true);
+                }}
+              />
             </div>
+          </div>
         </div>
-      </div>
 
-      <div className="flex-1">
-        <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Fund</h4>   
+        <div className="flex-1">
+          <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
+            Fund
+          </h4>
 
-        <div className="mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]">
-          <p className="font-epilogue fount-medium text-[20px] leading-[30px] text-center text-[#808191]">
-            Fund the campaign
-          </p>
-          <div className="mt-[30px]">
-            <input 
-              type="number"
-              placeholder="Amount"
-              step="0.01"
-              className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-            />
+          <div className="mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]">
+            <p className="font-epilogue fount-medium text-[20px] leading-[30px] text-center text-[#808191]">
+              Fund the campaign
+            </p>
+            <div className="mt-[30px]">
+              <input
+                type="number"
+                placeholder="Amount"
+                step="0.01"
+                className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
 
-            <div className="my-[20px] p-4 bg-[#13131a] rounded-[10px]">
-              <h4 className="font-epilogue font-semibold text-[14px] leading-[22px] text-white">Back it because you believe in it.</h4>
-              <p className="mt-[20px] font-epilogue font-normal leading-[22px] text-[#808191]">Support the project for no reward, just because it speaks to you.</p>
+              <div className="my-[20px] p-4 bg-[#13131a] rounded-[10px]">
+                <h4 className="font-epilogue font-semibold text-[14px] leading-[22px] text-white">
+                  Back it because you believe in it.
+                </h4>
+                <p className="mt-[20px] font-epilogue font-normal leading-[22px] text-[#808191]">
+                  Support the project for no reward, just because it speaks to
+                  you.
+                </p>
+              </div>
+
+              <CustomButton
+                btnType="button"
+                title="Fund Campaign"
+                styles="w-full bg-[#8c6dfd]"
+                handleClick={handleDonate}
+              />
             </div>
-
-            <CustomButton 
-              btnType="button"
-              title="Fund Campaign"
-              styles="w-full bg-[#8c6dfd]"
-              handleClick={handleDonate}
-            />
           </div>
         </div>
       </div>
     </div>
-  </div>
-)
-}
+  );
+};
 
-export default CampaignDetails
+export default CampaignDetails;
