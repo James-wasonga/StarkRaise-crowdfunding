f537142696cac3228c96b779795cf4ae72f84569
James-wasonga <jameswasonga22@gmail.com>
2024-08-12 22:07:22 +0300

    clean the profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,39 +1,3 @@
-// import React, { useState , useEffect} from 'react';
-
-// import { DisplayCampaigns } from '../components';
-// import { useStateContext } from '../context';
-
-// const Profile = () => {
-//   const [isLoading , setIsLoading] = useState(false);
-//   const [campaigns, setCampaigns] = useState([]);
-//   //Aiming at displaying the fetch data from the smart contract onto the homePage
-
-// const {address, contract, getUserCampaigns} = useStateContext();
-
-// const fetchCampaigns = async () => {
-//   setIsLoading(true);
-//   const data = await getUserCampaigns();
-//   setCampaigns(data);
-//   setIsLoading(false);
-// }
-
-
-// useEffect(() =>{
-//   if(contract) fetchCampaigns; 
-// },[address, contract]);
-
-//   return (
-//     <DisplayCampaigns
-//     title='All my Compaigns'
-//     isLoading = {isLoading}
-//     campaigns = {campaigns}
-//   />
-
-//   )
-// }
-
-// export default Profile
-
 
 import React, { useState , useEffect} from 'react';
 
