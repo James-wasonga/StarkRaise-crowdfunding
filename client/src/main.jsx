e01c909a9e8bfb98bebb674dda6ae65cd0bbb9bc
James-wasonga <jameswasonga22@gmail.com>
2024-09-02 21:34:42 +0300

    remove unused comments

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -27,26 +27,3 @@ root.render(
     
 
 
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import { StarknetProvider } from '@starknet-react/core';
-
-// import { StateContextProvider } from './context';
-// import App from './App';
-// import './index.css';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// root.render(
-//   <StarknetProvider>
-//     <Router>
-//       <StateContextProvider>
-//         <App />
-//       </StateContextProvider>
-//     </Router>
-//   </StarknetProvider>
-// );
-
-//this is the connection based on starknet
