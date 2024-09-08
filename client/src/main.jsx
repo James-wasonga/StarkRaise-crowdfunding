2b17e06039bd48da6c4e94c68bba3efeb122e0dc
James-wasonga <jameswasonga22@gmail.com>
2024-09-08 23:53:52 +0300

    comment unused code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
 import { StateContextProvider } from './context';
 import App from './App';
 import './index.css';
-
+//import AppProvider from './providers/AppProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
