e7965a3d5f051a51b13ba4633599c49fb820e99f
James-wasonga <jameswasonga22@gmail.com>
2025-05-28 10:27:07 +0300

    update campaign details

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -4,6 +4,7 @@ import { ethers } from "ethers";
 
 import { useStateContext } from "../context";
 import { CustomButton, CountBox, Loader, Modal } from "../components";
+import { ToastContainer, toast } from "react-toastify";
 
 import {
   bigintToLongAddress,
@@ -21,18 +22,21 @@ const CampaignDetails = () => {
     useAppContext();
   const [isLoading, setIsLoading] = useState(false);
   const [amount, setAmount] = useState("");
+  const [withdrawalAmount, setWithdrawalAmount] = useState("")
   const [donators, setDonators] = useState([]);
   const [withdrawModal, setWithdrawModal] = useState(false);
 
   const handleWithdrawFunction = () => {
     const campaignId = state.id.toString();
+    let _withdrawalAmount = withdrawalAmount * 10 ** 18;
     console.log(campaignId);
     if (contract && address) {
-      const myCall = contract.populate("withdraw_donations", [campaignId]);
+      const myCall = contract.populate("withdraw_donations", [campaignId, BigInt(_withdrawalAmount)]);
       setIsLoading(true);
       contract["withdraw_donations"](myCall.calldata)
         .then((res) => {
           console.info("Successful Response:", res);
+          toast.success("Withdrawal Successful", { position: "top-center", style: {backgroundColor: "#8c6dfd", color: "#fff", fontSize: "20px", width: "34vw", padding: "30px 20px"} })
         })
         .catch((err) => {
           console.error("Error: ", err);
@@ -69,65 +73,113 @@ const CampaignDetails = () => {
   const handleDonate = async () => {
     setIsLoading(true);
     await donate(state.id, amount);
-    navigate("/");
+    // navigate("/");
+
+    toast.success("Donation Successful", { position: "top-center", style: {backgroundColor: "#8c6dfd", color: "#fff", fontSize: "20px", width: "34vw", padding: "30px 20px"} })
     setIsLoading(false);
   };
 
   return (
     <div>
+      <ToastContainer />
       {isLoading && <Loader />}
 
       <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
-      <div className="flex-1 flex-col">
-        <img src={state.image} alt="campaign" className="w-full h-[410px] object-cover rounded-xl"/>
-        <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
-        { state && <div className="absolute h-full bg-[#4acd8d]" style={{ width: `${calculateBarPercentage(state.target.toString(), state.amount_collected.toString() / 1000000000000000000)}%`, maxWidth: '100%'}}>
-          </div>}
-        </div>
-      </div>
-
-      <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
-        <CountBox title="Days Left" value={remainingDays} />
-        <CountBox title={`Raised of ${state.target.toString()}`} value={state.amount_collected.toString() / 1000000000000000000 } />
-        <CountBox title="Total Backers" value={donators.length} />
-      </div>
-    </div>
-
-    <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
-      <div className="flex-[2] flex flex-col gap-[40px]">
-        <div>
-          <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Creator</h4>
-
-          <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
-            <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer">
-              <img src={stark} alt="user" className="w-[90%] h-[100%] object-contain"/>
-            </div>
-            <div>
-              <h4 className="font-epilogue font-semibold text-[14px] text-white break-all">{bigintToLongAddress(state.owner)}</h4>
-              <p className="mt-[4px] font-epilogue font-normal text-[12px] text-blue-500"> Campaigns</p>
-            </div>
+        <div className="flex-1 flex-col">
+          <img
+            src={state.image}
+            alt="campaign"
+            className="w-full h-[410px] object-cover rounded-xl"
+          />
+          <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
+            {state && (
+              <div
+                className="absolute h-full bg-[#4acd8d]"
+                style={{
+                  width: `${calculateBarPercentage(
+                    state.target.toString(),
+                    state.amount_collected.toString() / 1000000000000000000
+                  )}%`,
+                  maxWidth: "100%",
+                }}
+              ></div>
+            )}
           </div>
         </div>
 
-        <div>
-          <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Story</h4>
+        <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
+          <CountBox title="Days Left" value={remainingDays} />
+          <CountBox
+            title={`Raised of ${state.target.toString()}`}
+            value={state.amount_collected.toString() / 1000000000000000000}
+          />
+          <CountBox title="Total Backers" value={donators.length} />
+        </div>
+      </div>
+
+      <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
+        <div className="flex-[2] flex flex-col gap-[40px]">
+          <div>
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
+              Creator
+            </h4>
+
+            <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
+              <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer">
+                <img
+                  src={stark}
+                  alt="user"
+                  className="w-[100%] h-[100%] object-contain"
+                />
+              </div>
+              <div>
+                <h4 className="font-epilogue font-semibold text-[14px] text-white break-all">
+                  {bigintToLongAddress(state.owner)}
+                </h4>
+                <p className="mt-[4px] font-epilogue font-normal text-[12px] text-blue-500">
+                  {" "}
+                  Campaigns
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
+              Story
+            </h4>
 
             <div className="mt-[20px]">
-              <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">{state.description}</p>
+              <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">
+                {state.description}
+              </p>
             </div>
-        </div>
+          </div>
 
-        <div>
-          <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Donators</h4>
+          <div>
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
+              Donators
+            </h4>
 
             <div className="mt-[20px] flex flex-col gap-4">
-              {donators? donators.map((item, index) => (  
-                <div key={`${item.donor}-${index}`} className="flex justify-between items-center gap-4">
-                  <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">{index + 1}. {bigintToLongAddress(item.donor).slice(0,18) +"..."+bigintToLongAddress(item.donor).slice(-6) }</p>
-                  <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">{item.amount?.toString() / 1000000000000000000} Eth</p>
-                </div>
-              )) : (
-                <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">No donators yet. Be the first one!</p>
+              {donators ? (
+                donators.map((item, index) => (
+                  <div
+                    key={`${item.donor}-${index}`}
+                    className="flex justify-between items-center gap-4"
+                  >
+                    <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">
+                      {index + 1}. {bigintToLongAddress(item.donor).slice(0,18) +"..."+bigintToLongAddress(item.donor).slice(-6) }
+                    </p>
+                    <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">
+                      {item.amount?.toString() / 1000000000000000000} Eth
+                    </p>
+                  </div>
+                ))
+              ) : (
+                <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">
+                  No donators yet. Be the first one!
+                </p>
               )}
               <Modal
                 isOpen={withdrawModal}
@@ -143,11 +195,12 @@ const CampaignDetails = () => {
                   placeholder="Amount"
                   step="0.01"
                   className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value)}
+                  value={withdrawalAmount}
+                  onChange={(e) => setWithdrawalAmount(e.target.value)}
                 />
                 <br />
                 <br />
+
                 <CustomButton
                   handleClick={() => {
                     handleWithdrawFunction();
@@ -157,14 +210,16 @@ const CampaignDetails = () => {
                   styles="w-full bg-[#8c6dfd]"
                 />
               </Modal>
-              <CustomButton
-                btnType="button"
-                title="withdraw"
-                styles="w-full bg-[#8c6dfd]"
-                handleClick={() => {
-                  setWithdrawModal(true);
-                }}
-              />
+              {address == bigintToLongAddress(state.owner) && (
+                <CustomButton
+                  btnType="button"
+                  title="withdraw"
+                  styles="w-full bg-[#8c6dfd]"
+                  handleClick={() => {
+                    setWithdrawModal(true);
+                  }}
+                />
+              )}
             </div>
           </div>
         </div>
@@ -213,5 +268,3 @@ const CampaignDetails = () => {
 };
 
 export default CampaignDetails;
-
-
